f048f11d3a469941d340bf5fa0b3a30926b106cb
Ndaedzo Mudau <ndaedzomu101@gmail.com>
2026-02-23 09:50:52 +0200

    robot.txt

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portfolio-cache-v1';
+const CACHE_NAME = 'portfolio-cache-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -18,6 +18,15 @@ self.addEventListener('install', (event) => {
 
 // Fetch event: serve from cache if available, fallback to network
 self.addEventListener('fetch', (event) => {
+  // Ignore Live Server injected scripts and WebSockets
+  if (
+    event.request.url.includes('socket.io') ||
+    event.request.url.includes('/ws') ||
+    event.request.url.includes('reload.js')
+  ) {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
